0f062adafc71ed28a7d1ca845ced2687b4c374bf
choi sungmin <89989648+choisony@users.noreply.github.com>
2024-08-09 16:44:56 +0900

    Update App.vue

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <div>잼따</div>
+    <div>잼따~</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
